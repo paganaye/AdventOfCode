120e2fbab540b0e5dd3c4fb58c293bcae930137c
Pascal <pascal@ganaye.com>
2023-12-03 11:24:04 +0100

    Pascal Day3 Here is my day 3.

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,5 @@
+@file:Suppress("FunctionName", "unused")
+
 import `2023`.day1.darren.DataSource
 import `2023`.day1.darren.TrebuchetSolution
 
@@ -15,16 +17,20 @@ fun day1_Darren() {
 }
 
 fun main() {
-    println("** Day 1 - Darren")
-    day1_Darren()
-    println("** Day 1 - Pascal")
-    day1_Pascal()
+//    println("** Day 1 - Darren")
+//    day1_Darren()
+//    println("** Day 1 - Pascal")
+//    day1_Pascal()
+//
+//    println()
+//
+//    println("** Day 2 - Darren")
+//    println() // day2_Darren()
+//
+//    println("** Day 2 - Pascal")
+//    day2_Pascal()
 
     println()
-
-    println("** Day 2 - Darren")
-    println() // day2_Darren()
-
-    println("** Day 2 - Pascal")
-    day2_Pascal()
+    println("** Day 3 - Pascal")
+    day3_Pascal()
 }
