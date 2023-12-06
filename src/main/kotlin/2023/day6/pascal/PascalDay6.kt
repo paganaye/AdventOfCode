b50e61b1c523f332aadfcfb8c32114750c15ff44
Pascal <pascal@ganaye.com>
2023-12-06 14:46:01 +0100

    Pascal Day6 It was definitely easier

--- src/main/kotlin/2023/day6/pascal/PascalDay6.kt
+++ src/main/kotlin/2023/day6/pascal/PascalDay6.kt
@@ -1,34 +1,71 @@
-val day6PascalSampleDataPart1 = """"""
-val day6PascalMainData =
-    """"""
+val day6PascalSampleDataPart1 = """Time:      7  15   30
+Distance:  9  40  200
+"""
+val day6PascalSampleDataPart2 = """Time:        55     82     64     90
+Distance:   246   1441   1012   1111"""
 
+val day6PascalSampleDataPart3 = """Time:        55826490
+Distance:   246144110121111"""
+
+
+data class TimeAndDistance(val time: Long, val distance: Long)
 
 fun day6_Pascal() {
+    val spaceOrMore = Regex(" +")
 
+    fun parseTimeAndDistance(input: String): List<TimeAndDistance> {
+        val lines = input.lines()
+        val times = lines[0].split(spaceOrMore).drop(1).map { it.toLong() }
+        val distances = lines[1].split(spaceOrMore).drop(1).map { it.toLong() }
 
-    fun calculatePoints1(): Long {
-        return 0
+        return times.mapIndexed { index, time -> TimeAndDistance(time, distances[index]) }
     }
 
+    fun calculatePoints1(data: List<TimeAndDistance>): Long {
+        var result = 1L
 
-    fun calculatePoints2(): Long {
-        return 0
-    }
+        data.forEach {
+            val T = it.time
+            var D = it.distance
 
-    fun day6Part1(input: String): Long {
-        val result = calculatePoints1()
+            val a = -1.0
+            val b = T.toDouble()
+            val c = (-D).toDouble()
+            var sqrtDelta = Math.sqrt(b * b - 4 * a * c)
+            var x1 = (-b + sqrtDelta) / 2 * a
+            var x2 = (-b - sqrtDelta) / 2 * a
+            val i1 = kotlin.math.floor(x1)
+            val i2 = kotlin.math.floor(x2)
+
+            var count = i2 - i1
+            if (i1 == x1) count -= 1
+            result *= count.toLong()
+        }
+
         return result
     }
 
-    fun day6Part2(input: String): Long {
-        val result = calculatePoints2()
-        return 0
+
+//    fun calculatePoints2(data: List<TimeAndDistance>): Long {
+//        return 0
+//    }
+
+    fun day6Part1(input: String): Long {
+        val data = parseTimeAndDistance(input)
+        val result = calculatePoints1(data)
+        return result
     }
+
+//    fun day6Part2(input: String): Long {
+//        val data = parseTimeAndDistance(input)
+//        val result = calculatePoints2(data)
+//        return 0
+//    }
     println("  * Part 1")
-    println("    sample data: ${day6Part1(day6PascalSampleDataPart1)}")
-    println("    main data: ${day6Part1(day6PascalMainData)}")
+    println("    data: ${day6Part1(day6PascalSampleDataPart1)}")
     println("  * Part 2")
-    println("    sample data: ${day6Part2(day6PascalSampleDataPart1)}")
-    println("    main data: ${day6Part2(day6PascalMainData)}")
+    println("    data: ${day6Part1(day6PascalSampleDataPart2)}")
+    println("  * Part 3")
+    println("    data: ${day6Part1(day6PascalSampleDataPart3)}")
 
 }
